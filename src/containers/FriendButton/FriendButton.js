7492d9a5c240c6ea2d6f566f10b2f09a02ee917e
Majidsp <majid_sp2007@yahoo.com>
2019-10-31 11:36:59 +0100

    Better structured friendbutton component.

--- src/containers/FriendButton/FriendButton.js
+++ src/containers/FriendButton/FriendButton.js
@@ -3,68 +3,60 @@ import axios from "../../axios";
 
 const FriendButton = (props) => {
     const [buttonText, setButtonText] = useState('');
-    const [exist, setExist] = useState(false);
-    const [accepted, setAccepted] = useState(false);
-    const [receiver, setReceiver] = useState(false);
-    const [sender, setSender] = useState(false);
+    const [rerender, setRerender] = useState(false);
 
     const friendshipManager = () => {
-        if(!exist) {
+        if(buttonText == 'Send friend request') {
             sendRequest();
-        } else if (sender) {
+        } else if (buttonText == 'Cancel friend request') {
             cancelRequest();
-        } else if (receiver) {
+        } else if (buttonText == 'End friendship') {
+            cancelRequest();
+        } else if (buttonText == 'Accept friend request') {
             acceptRequest();
-        } else if (accepted) {
-            cancelRequest();
         }
     };
 
     const sendRequest = () => {
+        // (async () => {
+        //     await axios.post('/sendRequest', {receiver_id: props.userId});
+        //     setButtonText('');
+        // })();
         axios.post('/sendRequest', {receiver_id: props.userId})
-            .then(() => setButtonText(''))
+            .then(() => setRerender(!rerender))
             .catch(err => console.log(err));
     };
 
     const cancelRequest = () => {
         axios.post('/cancelRequest', {id: props.userId})
-            .then(() => setButtonText(''))
+            .then(() => setRerender(!rerender))
             .catch(err => console.log(err));
     };
 
     const acceptRequest = () => {
         axios.post('/acceptRequest', {id: props.userId})
-            .then(() => setButtonText(''))
+            .then(() => setRerender(!rerender))
             .catch(err => console.log(err));
     };
 
-
     useEffect(() => {
         (async () => {
-            console.log(props.userId);
             const { data } = await axios.post('/friendship', {receiver_id: props.userId});
             if(data.length > 0) {
-                setExist(true);
                 if(data[0].accepted) {
-                    setAccepted(true);
                     setButtonText('End friendship');
                 } else {
                     if(data[0].sender_id == props.userId) {
-                        setReceiver(true);
                         setButtonText('Accept friend request');
                     } else {
-                        setSender(true);
                         setButtonText('Cancel friend request');
                     }
                 }
-
             } else {
                 setButtonText('Send friend request');
             }
-
         })();
-
-    }, [buttonText]);
+    }, [rerender]);
 
     return (
         <div>
